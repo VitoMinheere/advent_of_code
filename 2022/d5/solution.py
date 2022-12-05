10a83a9a870b17d7143cf9adf898a7fd58d515ea
Vito Minheere <0959946+VitoMinheere@users.noreply.github.com>
2022-12-05 18:30:35 +0100

    Finish part 1

--- 2022/d5/solution.py
+++ 2022/d5/solution.py
@@ -1,39 +1,45 @@
 def p1(data):
-    stacks = {}
-    matrix = []
+    test_matrix = [
+        ["Z", "N"],
+        ["M", "C", "D"],
+        ["P"]
+    ]
+
+    matrix = [
+        ["Q", "M", "G", "C", "L"],
+        ["R", "D", "L", "C", "T", "F", "H", "G"],
+        ["V", "J", "F", "N", "M", "T", "W", "R"],
+        ["J", "F", "D", "V", "Q", "P"],
+        ["N", "F", "M", "S", "L", "B", "T"],
+        ["R", "N", "V", "H", "C", "D", "P"],
+        ["H", "C", "T"],
+        ["G", "S", "J", "V", "Z", "N", "H", "P"],
+        ["Z", "F", "H", "G"],
+    ]
+
+    code = ""
 
     for line in data:
-        # print(line)
-        items = line.split("  ")
-        if "move" not in items[0]:
-            cur_stack = 0
-            for crate in items:
-                if crate not in matrix and "[" in "".join(crate):
-                    matrix.insert(0, crate)
-                # for x in crate.split(" "):
-                #     if x == "":
-                #         # print("1234")
-                #         cur_stack += 1
-                #     elif "[" in crate:
-                #         stack = cur_stack // 2
-                #         if crate not in matrix:
-                #             matrix.insert(0, crate)
-                #         if (stack) not in stacks:
-                #             stacks[stack] = []
-                #         # print(cur_stack)
-                #         # print(crate)
-                #         if crate not in stacks[stack]:
-                #             stacks[stack].insert(0, crate)
-                        
-                # print(crate.split(" "))
-    print(stacks)
-    print(matrix)
+        s = line.split(" ")
+        am_crates = int(s[1])
+        stack = int(s[3]) - 1
+        dest = int(s[5]) - 1
+
+        for _ in range(0, am_crates):
+            origin = matrix[stack]
+            destination = matrix[dest]
+            destination.append(origin.pop(-1)) 
+
+    for x in matrix:
+        code += x[-1]
+
+    print("P1 = " + code)
             
 
 def p2(data):
     pass
 
-with open("test.txt") as t:
+with open("input.txt") as t:
     data = t.read().splitlines()
     p1(data)
     p2(data)
