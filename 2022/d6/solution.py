06507cf70e745fca1c85a2cce90d31d3a8006009
Vito Minheere <0959946+VitoMinheere@users.noreply.github.com>
2022-12-06 08:56:42 +0100

    Solution for day 6

--- 2022/d6/solution.py
+++ 2022/d6/solution.py
@@ -5,18 +5,29 @@ def p1(data):
         for x in range(3, len(line)):
             if line[x] not in read and (len(read) == len(set(read))):
                 print("Found mark ", line[x], " at char " + str(x + 1))
+                print("P1 = " + str(x+1))
                 read += line[x]
                 break
             else:
                 read += line[x]
                 read = read[1:]
-        print(read)
         
 
 def p2(data):
-    pass
+    for line in data:
+        start = line[0:13]
+        read = start
+        for x in range(13, len(line)):
+            if line[x] not in read and (len(read) == len(set(read))):
+                print("Found mark ", line[x], " at char " + str(x + 1))
+                print("P2 = " + str(x+1))
+                read += line[x]
+                break
+            else:
+                read += line[x]
+                read = read[1:]
 
 with open("input.txt") as t:
     data = t.read().splitlines()
     p1(data)
-    # p2(data)
+    p2(data)
