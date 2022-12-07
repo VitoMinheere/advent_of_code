9ed79013373e8b8489b1ef08fd6ffe54f6dac7dc
Vito Minheere <0959946+VitoMinheere@users.noreply.github.com>
2022-12-07 11:41:03 +0100

    Finish day 7

--- 2022/d7/solution.py
+++ 2022/d7/solution.py
@@ -1,82 +1,47 @@
 def p1(data):
     MAX_SIZE = 100_000
-    dirs = {"/": 0}
-    dir_contains = {}
-    sum_dirs = {}
+    dirs = {}
     dir_depth = []
     cur_dir = ""
-    cur_files = []
-    output = False
+    file_cnt = 0
     
     for i, line in enumerate(data):
         if line.startswith("$"): # It's a command
             if "cd" in line:
-                output = False
-                if cur_dir not in dirs:
-                    dirs[cur_dir] = 0 #cur_files
-
                 if line[-1] == ".":
                     cur_dir = dir_depth.pop()
                 else:
-                    cur_dir = line.split(" ")[-1]
+                    cur_dir = line.split(" ")[-1] + str(i) # Make each dir unique
+                    if cur_dir not in dirs:
+                        dirs[cur_dir] = 0
                     dir_depth.append(cur_dir)
-            elif "ls" in line:
-                output = True
-                continue
 
-        if output:
-            if line[0].isdigit():
-                size = int(line.split(" ")[0])
-                # print(line)
-                # print(dirs)
-                # print(size)
-                # print(dir_depth)
-                for dir in dir_depth:
-                    if dir in dirs:
-                        # print(dir)
-                        dirs[dir] += size
-                    else:
-                        dirs[dir] = size
+        elif line[0].isdigit():
+            file_cnt += 1
+            size = int(line.split(" ")[0])
+            for dir in dir_depth:
+                dirs[dir] += size
+        else:
+            continue
                         
-                # print(line)
-                # cur_files.append(int(line.split(" ")[0])) # Add file size
-            # elif "dir" in line:
-            #     for dir in dir_depth:
-            #         if dir in dir_contains:
-            #             dir_contains[dir].append(line.split(" ")[-1])
-            #         else:
-            #             dir_contains[dir] = [line.split(" ")[-1]]
-
-        # if (i == len(data)-1) and cur_dir not in dirs: # Also add last dir
-            # print("cur dir in cd = "+ cur_dir)
-            # dirs[cur_dir] = cur_files
-            # print(dirs[cur_dir])
-
-    # print(dirs)
-    # print(dir_contains)
-    # for k, v in dir_contains.items():
-    #     for x in v:
-    #         # print("DIR " + k)
-    #         # print(dirs[x])
-    #         dirs[k].extend(dirs[x])
-
-    # dirs_in_size = []
-    # for k, v in dirs.items():
-    #     s = sum(v)
-    #     print(f"dir {k} size = {str(s)}")
-    #     sum_dirs[k] = s
-    #     if s <= MAX_SIZE:
-    #         dirs_in_size.append(s)
-    print(dirs)
     p1 = sum([v for k, v in dirs.items() if v <= MAX_SIZE])
-
-    # print("P1 = " + str(sum(dirs_in_size)))
     print("P1 = " + str(p1))
+    return dirs
             
-def p2(data):
-    pass
+def p2(dirs):
+    DISK_SPACE = 70_000_000
+    USED = dirs["/0"]
+    UNUSED = DISK_SPACE - USED
+    NEEDED = 30_000_000
+    
+    options = []
+    for k, v in dirs.items():
+        if (v + UNUSED) > NEEDED:
+            options.append(v)
+    print(f"P2 = {sorted(options)[0]}") 
 
-with open("test.txt") as t:
+
+with open("input.txt") as t:
     data = t.read().splitlines()
-    p1(data)
-    # p2(data)
+    dirs = p1(data)
+    p2(dirs)
