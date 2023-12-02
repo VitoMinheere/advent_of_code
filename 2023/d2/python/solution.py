76bb366da97baa5d8a38a27c3fab1abd2c7dc50a
Vito Minheere <10959946+VitoMinheere@users.noreply.github.com>
2023-12-02 09:26:55 +0100

    Add part 2 of Day 2 in Python

--- 2023/d2/python/solution.py
+++ 2023/d2/python/solution.py
@@ -1,5 +1,6 @@
 import time
 import re
+from functools import reduce
 
 MAX_CUBES = {
    "red": 12,
@@ -22,7 +23,6 @@ def p1(data):
             for item in cubes:
                 amount, color = item.split(" ")
                 if int(amount) > MAX_CUBES[color]:
-                    print(f"{game_id} is impossible")
                     possible = False
                     break
 
@@ -33,7 +33,29 @@ def p1(data):
 
 
 def p2(data):
-    pass
+    answer = 0
+
+    for line in data:
+        game_id, revealed = line.split(": ")
+        game_id = int(game_id.split(" ")[-1])
+        revealed = revealed.split(";")
+
+        cubes_needed = {
+            "red": 0,
+            "green": 0,
+            "blue": 0
+        }
+
+        for part in revealed:
+            cubes = [x.lstrip() for x in part.split(",")]
+            for item in cubes:
+                amount, color = item.split(" ")
+                if int(amount) > cubes_needed[color]:
+                    cubes_needed[color] = int(amount)
+
+        answer += reduce(lambda x, y: x*y, cubes_needed.values())
+
+    return answer
 
 if __name__ == "__main__":
     with open("../input") as t:
@@ -45,8 +67,8 @@ if __name__ == "__main__":
         p1_time = time.time() - start_p1
         print("P1 took " + str(round(p1_time * 1000)) + " ms")
     
-        # start_p2 = time.time()
-        # answer_2 = p2(data)
-        # print(f"P2 answer = {answer_2}")
-        # p2_time = time.time() - start_p2
-        # print("P2 took " + str(round(p2_time * 1000)) + " ms")
+        start_p2 = time.time()
+        answer_2 = p2(data)
+        print(f"P2 answer = {answer_2}")
+        p2_time = time.time() - start_p2
+        print("P2 took " + str(round(p2_time * 1000)) + " ms")
