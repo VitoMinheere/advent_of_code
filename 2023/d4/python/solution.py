89ba6ca745e86a1e1670e5e13c3bb1a50e48dd8d
Vito Minheere <10959946+VitoMinheere@users.noreply.github.com>
2023-12-04 07:58:26 +0100

    Add solution part 2 day 4

--- 2023/d4/python/solution.py
+++ 2023/d4/python/solution.py
@@ -1,6 +1,4 @@
 import time
-import re
-from functools import reduce
 
 def p1(data):
     answer = 0
@@ -21,12 +19,34 @@ def p1(data):
    
     return answer
 
+
 def p2(data):
-    pass
+    answer = 0
+
+    hand = {}
+    cards = {} 
+    
+    for i, line in enumerate(data):
+        card_number, rest = line.split(": ")
+        win_nums, own_nums = rest.split("|")
+        win_nums = [x for x in win_nums.replace("  ", " ").split(" ") if x.isnumeric()]
+        own_nums = [x for x in own_nums.replace("  ", " ").split(" ") if x.isnumeric()]
+
+        points_in_card = len([x for x in own_nums if x in win_nums])
+
+        cards[i] = {"copies": 1, "wins": points_in_card}
+    
+    for i, v in cards.items():
+        for y in range(v["copies"]):
+            answer += 1
+            for w in range(i, i+v["wins"]):
+                cards[w+1]["copies"] += 1
+
+   
+    return answer
 
 if __name__ == "__main__":
     with open("../input") as t:
-    # with open("2023/d3/input") as t:
         data = t.read().splitlines()
 
         start_p1 = time.time()
@@ -35,8 +55,8 @@ if __name__ == "__main__":
         p1_time = time.time() - start_p1
         print("P1 took " + str(round(p1_time * 1000)) + " ms")
     
-        # start_p2 = time.time()
-        # answer_2 = p2(data, True)
-        # print(f"P2 answer = {answer_2}")
-        # p2_time = time.time() - start_p2
-        # print("P2 took " + str(round(p2_time * 1000)) + " ms")
+        start_p2 = time.time()
+        answer_2 = p2(data)
+        print(f"P2 answer = {answer_2}")
+        p2_time = time.time() - start_p2
+        print("P2 took " + str(round(p2_time * 1000)) + " ms")
