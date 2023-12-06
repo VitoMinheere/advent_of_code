10e7d4f17c94365e7fbba7ad4dec9c9c25bbe354
Vito Minheere <10959946+VitoMinheere@users.noreply.github.com>
2023-12-06 08:34:57 +0100

    Solution day 6 p2 in python but slow

--- 2023/d6/python/solution.py
+++ 2023/d6/python/solution.py
@@ -3,7 +3,7 @@ from functools import reduce
 
 
 
-def solution(data, p2=False):
+def p1(data):
     times = []
     distances = []
     for line in data:
@@ -14,9 +14,6 @@ def solution(data, p2=False):
 
     options_per_race = []
     for i in range(0, len(times)):
-        # print(f"Race {i}")
-        # print(f"Time {times[i]}")
-        # print(f"Distance {distances[i]}")
 
         winning_options = 0
         for s in range(1, times[i]+1):
@@ -30,22 +27,42 @@ def solution(data, p2=False):
     answer = reduce(lambda x, y: x*y, options_per_race)
     return answer
 
+def p2(data):
+    time = 0
+    distance = 0
+    for line in data:
+        if line.startswith("Time"):
+            time = int("".join([x for x in line.split(": ")[-1].split(" ") if x.isnumeric()]))
+        elif line.startswith("Distance"):
+            distance = int("".join([x for x in line.split(": ")[-1].split(" ") if x.isnumeric()]))
+
+    
+    winning_options = 0
+    for s in range(1, time+1):
+        speed = 1 * s
+        time_to_move = time - s
+        dist = speed * time_to_move
+        if dist > distance:
+            winning_options += 1
+
+    answer = winning_options
+    return answer
+
             
 
 
 if __name__ == "__main__":
-    # with open("../test") as t:
     with open("2023/d6/input") as t:
         data = t.read().splitlines()
 
         start_p1 = time.time()
-        answer_1 = solution(data)
+        answer_1 = p1(data)
         print(f"P1 answer = {answer_1}")
         p1_time = time.time() - start_p1
         print("P1 took " + str(round(p1_time * 1000)) + " ms")
     
-        # start_p2 = time.time()
-        # answer_2 = solution(data, True)
-        # print(f"P2 answer = {answer_2}")
-        # p2_time = time.time() - start_p2
-        # print("P2 took " + str(round(p2_time * 1000)) + " ms")
+        start_p2 = time.time()
+        answer_2 = p2(data)
+        print(f"P2 answer = {answer_2}")
+        p2_time = time.time() - start_p2
+        print("P2 took " + str(round(p2_time * 1000)) + " ms")
